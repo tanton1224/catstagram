9025f5cabea03f5131695a99156060322a965b03
Thomas Anton <thomasanton1224@gmail.com>
2022-06-08 14:18:09 -0400

    catstagram

--- public/index.js
+++ public/index.js
@@ -1,19 +1,39 @@
 // Your code here
 window.onload = async () => {
+  const mainContainer = document.createElement('div');
+  mainContainer.setAttribute('class', 'main-container')
+  document.body.append(mainContainer);
+
   const title = document.createElement("h1")
   title.innerText = "Kitten Pic"
 
 
-  let imageDiv = document.createElement("div")
-  imageDiv.setAttribute("class", "post")
-  let res = await fetch("https://api.thecatapi.com/v1/images/6pKEmaQ0X");
-  let data = await res.json();
-  let url = data.url;
-  const image = document.createElement("img")
-  image.setAttribute("src", url)
-  imageDiv.append(image)
+  let postDiv = document.createElement("div")
+  postDiv.setAttribute("class", "post")
 
-  document.body.append(title);
-  document.body.append(imageDiv);
+  let res = await fetch("https://api.thecatapi.com/v1/images/search/");
+  let data = await res.json();
+
+  const image = document.createElement("img")
+  image.setAttribute("src", `${data[0].url}`)
+  image.setAttribute("class", 'cat-picture')
+  postDiv.append(image)
+
+  mainContainer.append(title);
+  mainContainer.append(postDiv);
+
+
+  let newCatButton = document.createElement('button');
+  newCatButton.innerText = 'Get New Cat'
+  mainContainer.append(newCatButton)
+
+  getNewCat = async () => {
+    let newCat = await fetch("https://api.thecatapi.com/v1/images/search/");
+    let newCatData = await newCat.json();
+    image.setAttribute('src', `${newCatData[0].url}`)
+  }
+
+
+  newCatButton.addEventListener('click', getNewCat)
 
 }
